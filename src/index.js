9d95571aada15b1ae6ea773eb1bf5a31b4f481b0
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-08-29 11:54:44 -0300

    feat: atualizando código

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-
 const app = express();
-
 app.use(morgan('dev'));
 
 app.get('/ping', (req, res) => {
 	res.send('Pong!');
 });
 
-app.listen(process.env.PORT || 2222, () => {
-	console.log('Server running on port 2222');
+app.listen(process.env.PORT, () => {
+	console.log(`${process.env.PORT} e a mensagem: ${process.env.MESSAGE}`);
 });
